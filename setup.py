93a9926bc5292a9c958ab03065eca0b608700214
manova <manova.abraham@gmail.com>
2024-06-24 13:13:30 +0530

    adding information in setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        return req.read().splitlines()
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=read_requirements(),
+    author='Your Name',  # Replace with your name
+    author_email='immanuel2504@gmail.com',
+)
